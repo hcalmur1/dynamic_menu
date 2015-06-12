91228b0071cb643b73979436f928731df43a7f3e
hcalmur1 <hcalmur@gmail.com>
2015-06-11 19:01:20 -0500

    Versión final del menu dynamic

--- index.php
+++ index.php
@@ -1,26 +1,26 @@
 <?php
-	//Set the database connection
-	mysql_connect('localhost','root','');
-	mysql_select_db('test');
-	//select all rows from the main_menu table
-	$result = mysql_query("select id,title,parentid,link from main_menu");
+//Set the database connection
+mysql_connect('localhost', 'root', 'root');
+mysql_select_db('test');
+//select all rows from the main_menu table
+$result = mysql_query("select id,title,parentid,link from main_menu");
 
-	//create a multidimensional array to hold a list of menu and parent menu
-	$menu = array(
-		'menus' => array(),
-		'parent_menus' => array()
-	);
+//create a multidimensional array to hold a list of menu and parent menu
+$menu = array(
+	'menus' => array(),
+	'parent_menus' => array(),
+);
 
-	//build the array lists with data from the menu table
-	while ($row = mysql_fetch_assoc($result)) {
-		//creates entry into menus array with current menu id ie. $menus['menus'][1]
-		$menu['menus'][$row['id']] = $row;
-		//creates entry into parent_menus array. parent_menus array contains a list of all menus with children
-		$menu['parent_menus'][$row['parentid']][] = $row['id'];
-	}
-	
-	// Create the main function to build milti-level menu. It is a recursive function.	
-	function buildMenu($parent, $menu) {
+//build the array lists with data from the menu table
+while ($row = mysql_fetch_assoc($result)) {
+	//creates entry into menus array with current menu id ie. $menus['menus'][1]
+	$menu['menus'][$row['id']] = $row;
+	//creates entry into parent_menus array. parent_menus array contains a list of all menus with children
+	$menu['parent_menus'][$row['parentid']][] = $row['id'];
+}
+
+// Create the main function to build milti-level menu. It is a recursive function.
+function buildMenu($parent, $menu) {
 	$html = "";
 	if (isset($menu['parent_menus'][$parent])) {
 		$html .= "<ul>";
@@ -53,7 +53,7 @@
 <body>
 
 <div id='cssmenu'>
-<?php echo buildMenu(0, $menu);; ?>
+<?php echo buildMenu(0, $menu);?>
 </div>
 
 </body>
